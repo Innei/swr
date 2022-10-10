bbc90f163835a58369a77c9033108367a80183da
Innei <tukon479@gmail.com>
2022-10-10 11:19:18 +0800

    chore: example
    Signed-off-by: Innei <tukon479@gmail.com>

--- example/App.tsx
+++ example/App.tsx
@@ -22,12 +22,20 @@ const App: FC = () => {
   const { data } = useSWR(['/api'], fetcher, {
     initialData: { message: 'initial' },
   })
-
   const handleClick = () => {
     swr(['fetch-btn'], fetcher).then((res) => {
       console.log('fetch done', res)
     })
   }
+
+  const handleClickCache = () => {
+    swr(['fetch-btn'], fetcher, {
+      maxAge: 3000,
+    }).then((res) => {
+      console.log('fetch done', res)
+    })
+  }
+
   return (
     <div>
       <h5>useSWR</h5>
@@ -35,7 +43,14 @@ const App: FC = () => {
 
       <h5>SWR</h5>
       <div>
-        <button onClick={handleClick}>fetch data</button>
+        <button onClick={handleClick}>
+          fetch data with 1s waiting and no cache
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClickCache}>
+          fetch data with 1s waiting and 3s cache
+        </button>
       </div>
     </div>
   )
